5bf69db03f889ef19006ff89e600899ac0fbae61
bastrees <bastidait@yahoo.com>
2024-07-08 08:38:36 +0800

    updated for admin appoval for new users

--- server/routes/User.routes.js
+++ server/routes/User.routes.js
@@ -5,10 +5,12 @@ const UserController = require('../controller/User.controller');
 
 router.post('/loginuser', UserController.LoginUser);
 router.post('/createuser', UserController.CreateUser);
-router.get('/users', UserController.ListUsers); 
+router.get('/users', UserController.ListUsers);
 router.put('/users/:id', UserController.EditUser);
-router.delete('/users/:id', UserController.DeleteUser); 
+router.delete('/users/:id', UserController.DeleteUser);
 router.get('/users/managers', UserController.ListManagers);
-router.get('/users/customers', UserController.ListCustomers); 
+router.get('/users/customers', UserController.ListCustomers);
+router.get('/users/pending', UserController.ListPendingUsers);
+router.put('/users/approve/:id', UserController.ApproveUser);
 
 module.exports = router;
