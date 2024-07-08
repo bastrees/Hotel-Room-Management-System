e813925c3d67f34643f19f76abede3237d553835
bastrees <bastidait@yahoo.com>
2024-07-08 22:49:37 +0800

    added a few routes for customer handling

--- dashboard/src/routes.jsx
+++ dashboard/src/routes.jsx
@@ -3,13 +3,15 @@ import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-import Dashboard from './pages/customer/Dashboard';
+import CustomerDashboard from './pages/customer/CustomerDashboard';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import ManagerDashboard from './pages/manager/ManagerDashboard';
 import UserManagement from './pages/admin/UserManagement';
 import Reports from './pages/admin/Reports';
 import RoomManagementPage from './pages/admin/RoomManagementPage';
 import ManagerRoomManagementPage from './pages/manager/RoomManagementPage';
+import RoomSearch from './components/Customer/RoomSearch';
+import BookingHistory from './components/Customer/BookingHistory';
 import ProtectedRoute from './ProtectedRoute';
 import Layout from './components/Layout';
 import { roles } from './roles';
@@ -28,10 +30,26 @@ const Routes = createBrowserRouter([
         element: <Layout><SignUp /></Layout>,
     },
     {
-        path: '/dashboard',
+        path: '/customer',
         element: (
-            <ProtectedRoute allowedRoles={[roles.CUSTOMER, roles.MANAGER, roles.ADMIN]}>
-                <Layout><Dashboard /></Layout>
+            <ProtectedRoute allowedRoles={[roles.CUSTOMER]}>
+                <Layout><CustomerDashboard /></Layout>
+            </ProtectedRoute>
+        ),
+    },
+    {
+        path: '/customer/room-search',
+        element: (
+            <ProtectedRoute allowedRoles={[roles.CUSTOMER]}>
+                <Layout><RoomSearch /></Layout>
+            </ProtectedRoute>
+        ),
+    },
+    {
+        path: '/customer/booking-history',
+        element: (
+            <ProtectedRoute allowedRoles={[roles.CUSTOMER]}>
+                <Layout><BookingHistory /></Layout>
             </ProtectedRoute>
         ),
     },
